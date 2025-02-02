4a6e0e3dac96e1a901951ce6caa818a91da6217c
flori <hamflb19@htl-kaindorf.at>
2025-02-02 19:36:18 +0100

    fixed player first equipping lvl 2 instead of 1

--- src/main/java/at/flori4n/gungame/GungamePlayer.java
+++ src/main/java/at/flori4n/gungame/GungamePlayer.java
@@ -20,7 +20,7 @@ public class GungamePlayer {
 
     public GungamePlayer(Player player) {
         this.player = player;
-        gunGameData.getKits().get(lvl).applyKit(player);
+        equip();
     }
     public void equip(){
         //-1 cuz indexing starts at 0
@@ -37,21 +37,5 @@ public class GungamePlayer {
     }
     public void lvlDown(){
         if (lvl>1)lvl--;
-    }
-
-    protected int calcXPLevels (int levels) {
-        int xp = 0;
-
-        for (int i = 1; i <= levels; i++) {
-            if (i <= 16) {
-                xp += 17;
-            } else if (i > 16 && i <= 31) {
-                xp += (i - 16) * 3 + 17;
-            } else if (i > 31) {
-                xp += (i - 31) * 7 + 62;
-            }
-        }
-
-        return xp;
     }
 }
